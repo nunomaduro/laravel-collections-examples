12bfc3492b1c0f3b0e9fb98cecee2de8bec2555a
Nuno Maduro <nunomaduro@Nunos-MacBook-Pro.local>
2016-08-22 00:31:09 +0200

    Updates bootstrap dataset

--- src/examples/bootstrap.php
+++ src/examples/bootstrap.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Collection;
 use App\Person;
 
 $arrayVariable = [
-	new Person('Nuno Maduro', 26, 'CTO', 2600),
+	new Person('Nuno Maduro', 26, 'CTO', 5000),
 	new Person('Alex', 31, 'Lead Developer', 2000),
 	new Person('Fabien', 44, 'Front End Developer', 1900),
 	new Person('Kevin', 25, 'Web Developer', 1000),
